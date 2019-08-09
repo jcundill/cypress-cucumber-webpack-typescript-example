00894fce1d69cc38c1537d7e542fc10c65577782
jcundill <jcundill@barracuda.com>
2019-08-09 21:07:23 +0100

    generate cucumber.json as a node task. This avoids the needs for mocks in webpack.config.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,12 @@
 const webpack = require("@cypress/webpack-preprocessor");
+const { generateCucumberJson } = require("cypress-cucumber-preprocessor").generateCucumberJson; // eslint-disable-line
 
 module.exports = on => {
   const options = {
     webpackOptions: require("../webpack.config.js")
   };
   on("file:preprocessor", webpack(options));
+  on("task", {
+    cukejson: generateCucumberJson
+  });
 };
